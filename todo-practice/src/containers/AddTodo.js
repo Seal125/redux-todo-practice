56d9eaff9ae664346042bab623e647cc566b2735
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-06-09 21:00:29 -0400

    Clears input field on focus

--- todo-practice/src/containers/AddTodo.js
+++ todo-practice/src/containers/AddTodo.js
@@ -5,20 +5,24 @@ import { addTodo } from "../actions";
 const AddTodo = ({ dispatch }) => {
   const [input, setInput] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!input) {
+      return;
+    }
+    dispatch(addTodo(input));
+    setInput("");
+  };
+
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          if (!input) {
-            return;
-          }
-          dispatch(addTodo(input));
-          setInput("");
-        }}
-      >
-        <input type='text' onChange={(e) => setInput(e.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onFocus={(e) => (e.target.value = "")}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type='submit'>Add Todo</button>
       </form>
     </div>
